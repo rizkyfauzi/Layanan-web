7e5a210adf30892eae10c6b6e70aa51659730b5d
137006237 <137006237@student.unsil.ac.id>
2016-04-10 00:25:05 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'isikan consumer_key nya');
+define('CONSUMER_SECRET', 'isikan consumer_secret nya');
+define('OAUTH_CALLBACK', 'isikan callback nya');
+?>
