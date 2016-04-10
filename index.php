47c5276aaf04fd1eccaebc32da09bce594f9ef4c
Muhamad Rizky Fauzi <137006238@student.unsil.ac.id>
2016-04-10 15:26:48 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<title> Twitter </title>
+<head><p> <center> <h1> Silahkan Anda Log In</h1> </center> </p> </head>
+
+<br>
+<table width=20% border=1 align="center" bgcolor="" =>
+  <tr>
+    <th colspan=2> Log In</th>
+  </tr>
+  <tr>
+    <td>Username</td>
+    <td>19 - 08 - 1997</td>
+  </tr>
+  <tr>
+  <td>Password</td>
+  
+  </tr>
+</table>
+<font size=5 font face="calibri" it font color=#ffffff valign="top"><div class="container-fluid">
+<body>
+</body>
+</html>
