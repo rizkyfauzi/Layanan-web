9189989b2ec5b0cea98fe6f49ec61353ba1bcd04
137006237 <137006237@student.unsil.ac.id>
2016-04-10 11:59:18 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
-?php
+<?php
 
 /**
  * @file
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'isikan consumer_key nya');
-define('CONSUMER_SECRET', 'isikan consumer_secret nya');
-define('OAUTH_CALLBACK', 'isikan callback nya');
+define('CONSUMER_KEY', 'Rf59DvzBVXgymmKeDZZ3bnvxw');
+define('CONSUMER_SECRET', 'B8FPHtT6ENdgoc7ieuKd8czq45snzwLaE1xEnGTdoNIK23yLYt');
+define('OAUTH_CALLBACK', 'http://localhost/twitter2/callback.php');
 ?>
