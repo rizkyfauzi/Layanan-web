7b195215e67d72469c399e6a35ff5906f95c0376
137006237 <137006237@student.unsil.ac.id>
2016-04-23 22:26:35 +0700

    Create twitteroauth.php

---
+++ twitteroauth.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Abraham Williams (abraham@abrah.am) http://abrah.am
+ *
+ * The first PHP Library to support OAuth for Twitter's REST API.
+ */
+
+/* Load OAuth lib. You can find it at http://oauth.net */
+require_once('OAuth.php');
+
+/**
+ * Twitter OAuth class
+ */
